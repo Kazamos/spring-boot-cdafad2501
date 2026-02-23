f7abe16c8183f7cf8e7e9301f0257554e294a8df
mithridatem <mathieumithridate@adrar-formation.com>
2026-02-23 14:02:27 +0100

    feat : base project + h2 console

---
+++ src/main/java/com/adrar/cdafad/CdafadApplication.java
@@ -0,0 +1,13 @@
+package com.adrar.cdafad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CdafadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CdafadApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/adrar/cdafad/CdafadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.adrar.cdafad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CdafadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
