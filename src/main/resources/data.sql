603e005a809a3c3039b6904c2829877dc7f48528
mithridatem <mathieumithridate@adrar-formation.com>
2026-02-23 14:11:53 +0100

    feat : initialisation BDD H2

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO utilisateur (firstname, lastname, email, password) VALUES
+('Mathieu', 'Mithridate', 'mathieumith@test.com', '1234'),
+('Jean', 'Albert', 'jeanalbert@test.fr', '1234'),
+('Marie', 'Durand', 'mariedurand@mail.com', '1234');
