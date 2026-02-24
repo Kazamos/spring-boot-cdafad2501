012166436f32b8f7b7905651439e0edc27273704
mithridatem <mathieumithridate@adrar-formation.com>
2026-02-24 10:27:25 +0100

    feat : add all repository + category service

---
+++ src/main/java/com/adrar/cdafad/repository/ManufacturerRepository.java
@@ -0,0 +1,9 @@
+package com.adrar.cdafad.repository;
+
+import com.adrar.cdafad.entity.Manufacturer;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ManufacturerRepository extends CrudRepository<Manufacturer, Integer> {
+}
---
+++ src/main/java/com/adrar/cdafad/service/CategoryService.java
@@ -0,0 +1,50 @@
+package com.adrar.cdafad.service;
+
+import com.adrar.cdafad.entity.Category;
+import com.adrar.cdafad.repository.CategoryRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@AllArgsConstructor
+public class CategoryService {
+
+    private CategoryRepository categoryRepository;
+
+    //Méthodes du service
+    //Méthode qui ajoute une category
+    public Category createCategory(Category category) throws Exception {
+        try {
+            this.isCategoryExistsByName(category.getName());
+        } catch (Exception e)
+        {
+            throw new Exception(e.getMessage());
+        }
+        return categoryRepository.save(category);
+    }
+
+    //Méthode qui retourne une category par son ID
+    public Optional<Category> getCategoryById(Integer id) throws Exception {
+        //Récupération de la category (findById du repository)
+        Optional<Category> category = categoryRepository.findById(id);
+        //Test si elle n'existe pas
+        if (!category.isPresent()) {
+            throw new Exception("La category n'existe pas");
+        }
+        return category;
+    }
+
+    //méthode qui retourne la liste de toutes les categories
+
+    //méthode pour retourner une category par son name
+    private boolean isCategoryExistsByName(String name) throws Exception {
+        //Test si la category existe
+        if(categoryRepository.existsByName(name)) {
+            throw new Exception("La category " + name + " existe déja");
+        }
+        return true;
+    }
+}
